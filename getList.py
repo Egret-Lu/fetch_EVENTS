17e59aad19b5a3ba37f8b30ac2e07ff31ce9556c
lucinda <luxinxuan0312@look.com>
2022-07-08 10:36:23 -0400

    daily submit 0708

--- getList.py
+++ getList.py
@@ -1,11 +1,12 @@
 # %% codecell 
+## Create evlist.txt file for ASWMS to use
 from setup_parameters import *
 import os
 import sys
 from os import listdir
 f=open(search_dir+'evlist.txt','w')
 # %% codecell
-#get event list form file
+#get event list from file
 for event in listdir(search_dir):
     if event[0:2] == '20':
         f.write(event+"\n")
